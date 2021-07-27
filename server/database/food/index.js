d9911dc70b231c9b93627c612bb667116971a98e
siddharthkm121 <siddharthkm121@gmail.com>
2021-07-27 18:40:43 +0530

    adding API

--- server/database/food/index.js
+++ server/database/food/index.js
@@ -1,26 +1,32 @@
 import mongoose, { mongo } from "mongoose";
 
-const foodSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    descript: { type: String, required: true },
-    isVeg: { type: Boolean, required: true },
-    isContainsEgg: { type: Boolean, required: true },
-    category: { type: String, required: true },
-    photos: {
-        type: mongoose.Types.ObjectId,
-        ref: "Images",
+const FoodSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        descript: { type: String, required: true },
+        isVeg: { type: Boolean, required: true },
+        isContainsEgg: { type: Boolean, required: true },
+        category: { type: String, required: true },
+        photos: {
+            type: mongoose.Types.ObjectId,
+            ref: "Images",
+        },
+        price: { type: Number, default: 150, required: true },
+        addOns: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "Foods",
+            },
+        ],
+        restaurant: {
+            type: mongoose.Types.ObjectId,
+            ref: "Restaurants",
+            required: true,
+        },
     },
-    price: { type: Number, defaut: 150, require: true },
-    addOns: [{
-        type: mongoose.Types.ObjectId,
-        ref: "Foods",
-    },
-    ],
-    restaurant: {
-        type: mongoose.Types.ObjectId,
-        ref: "Restaurants",
-        required: true,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
-export const FoodModel = mongo.model("Foods", FoodSchema);
+export const FoodModel = mongoose.model("Foods", FoodSchema);
--- server/database/restaurant/index.js
+++ server/database/restaurant/index.js
@@ -1,27 +1,32 @@
-import Mongoose from "mongoose";
+import mongoose from "mongoose";
 
-const RestaurantSchema = new Mongoose.Schema({
-    name: { Type: String, required: true },
-    city: { type: String, required: true },
-    address: { type: String, required: true },
-    mapLocation: { type: String, required: true },
-    cuisine: [String],
-    restaurantTimings: String,
-    contactNumber: Number,
-    website: Number,
-    popularDishes: [String],
-    averageCost: Number,
-    amenties: [String],
-    menuImages: {
-        type: mongoose.Types.ObjectID,
-        ref: " Images",
+const RestaurantSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        city: { type: String, required: true },
+        address: { type: String, required: true },
+        mapLocation: { type: String, required: true },
+        cuisine: [String],
+        restaurantTimings: String,
+        contactNumber: Number,
+        website: Number,
+        popularDishes: [String],
+        averageCost: Number,
+        amenties: [String],
+        menuImages: {
+            type: mongoose.Types.ObjectId,
+            ref: "Images",
+        },
+        menu: {
+            type: mongoose.Types.ObjectId,
+            ref: "Menus",
+        },
+        reviews: [{ type: mongoose.Types.ObjectId, ref: "Reviews" }],
+        photos: { type: mongoose.Types.ObjectId, ref: "Images" },
     },
-    menu: {
-        type: mongoose.Types.ObjectID,
-        ref: " Menus",
-    },
-    reviews: [{ type: mongoose.Types.ObjectID, ref: "Reviews" }],
-    photos: { type: mongoose.Types, ObjectID, ref: "Images" },
-});
+    {
+        timestamps: true,
+    }
+);
 
 export const RestaurantModel = mongoose.model("Restaurants", RestaurantSchema);
