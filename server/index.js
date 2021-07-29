b3e733b71c2785ced074016f7cfea2249bc81d4a
siddharthkm121 <siddharthkm121@gmail.com>
2021-07-29 15:12:39 +0530

    adding google directory

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
+
+// configs
+import googleAuthConfig from "./config/google.config";
 
 // microservice routes
 import Auth from "./API/Auth";
@@ -19,6 +23,11 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+// passport cofiguration
+googleAuthConfig(passport);
 
 // Application Routes
 zomato.use("/auth", Auth);
@@ -31,4 +40,4 @@ zomato.listen(4000, () =>
         .catch(() =>
             console.log("Server is running, but database connection failed... ")
         )
-);
+)
